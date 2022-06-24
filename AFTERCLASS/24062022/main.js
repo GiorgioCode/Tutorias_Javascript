692c13900684b3a3ce7dd77368679aa9664c638b
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2022-06-24 01:41:25 -0300

    condicional favoritos

--- AFTERCLASS/24062022/main.js
+++ AFTERCLASS/24062022/main.js
@@ -1,8 +1,8 @@
-
 //IDENTIFICA ELEMENTO CONTENEDOR O PADRE PARA INSERTAR TARJETAS
 let contenedor = document.getElementById("container");
 //GENERADOR DE TARJETAS DE USUARIOS
-user.forEach((user) => { //BUCLE QUE REPASA LOS OBJETOS DE USER EN ARCHIVO usuarios.js
+user.forEach((user) => {
+  //BUCLE QUE REPASA LOS OBJETOS DE USER EN ARCHIVO usuarios.js
   contenedor.innerHTML += `
   <div class="card text-white bg-primary m-3"> 
 	<div class="card-header"><img src=${user.avatar} /> ${user.first_name} ${user.last_name}</div>
@@ -15,47 +15,47 @@ user.forEach((user) => { //BUCLE QUE REPASA LOS OBJETOS DE USER EN ARCHIVO usuar
   </div>`; //cada tarjeta se va a generar ubicando las propiedades de cada objeto segun la obicacion determinada
 });
 
-
 //BUSCADOR DE USUARIOS
 function filtroUsuarios() {
-	const entrada = document.querySelector(".buscarUsuarios");
-	entrada.addEventListener("keyup", (e) => {
-    console.log(e)
+  const entrada = document.querySelector(".buscarUsuarios");
+  entrada.addEventListener("keyup", (e) => {
+    console.log(e);
 
-		let entradaMinuscula = e.target.value.toLowerCase(); //manda a minusculas las entradas del input
-		console.log(entradaMinuscula)
-		if (e.target.matches(".buscarUsuarios")) {
-			document.querySelectorAll(".card").forEach(
-				(el) =>	el.textContent.toLowerCase().includes(entradaMinuscula) //manda a minusculas el contenido de las tarjetas y compara con el input
-					? el.classList.remove("filtrar") //visibiliza tarjetas
-					: el.classList.add("filtrar") //esconde tarjetas
-					);
-				}
-			});
-		}
-		
-		filtroUsuarios()
-		
-		let contenedor_fav = document.getElementById("container_fav");
-		//FUNCION SELECTORA DE USUARIOS
+    let entradaMinuscula = e.target.value.toLowerCase(); //manda a minusculas las entradas del input
+    console.log(entradaMinuscula);
+    if (e.target.matches(".buscarUsuarios")) {
+      document.querySelectorAll(".card").forEach(
+        (el) =>
+          el.textContent.toLowerCase().includes(entradaMinuscula) //manda a minusculas el contenido de las tarjetas y compara con el input
+            ? el.classList.remove("filtrar") //visibiliza tarjetas
+            : el.classList.add("filtrar") //esconde tarjetas
+      );
+    }
+  });
+}
 
-let favoritos =[];
+filtroUsuarios();
+
+let contenedor_fav = document.getElementById("container_fav");
+//FUNCION SELECTORA DE USUARIOS
+
+let favoritos = [];
 
 function cargarFavoritos() {
-	if (localStorage.getItem("StorageFavoritos") !== null) {
-		favoritos = JSON.parse(localStorage.getItem("StorageFavoritos"));
-		return;
-	} else {
-		localStorage.setItem("StorageFavoritos", JSON.stringify(favoritos));
-		return;
-	}
-};
+  if (localStorage.getItem("StorageFavoritos") !== null) {
+    favoritos = JSON.parse(localStorage.getItem("StorageFavoritos"));
+    return;
+  } else {
+    localStorage.setItem("StorageFavoritos", JSON.stringify(favoritos));
+    return;
+  }
+}
 
-cargarFavoritos()
+cargarFavoritos();
 
 //GENERADOR DE TARJETAS DE USUARIOS
 favoritos.forEach((favoritos) => {
-	contenedor_fav.innerHTML += `
+  contenedor_fav.innerHTML += `
 	<div class="card text-white bg-primary m-3"> 
 	  <div class="card-header"><img src=${favoritos.avatar} /> ${favoritos.first_name} ${favoritos.last_name}</div>
 	  <div class="card-body">
@@ -67,36 +67,32 @@ favoritos.forEach((favoritos) => {
 	</div>`; //cada tarjeta se va a generar ubicando las propiedades de cada objeto segun la obicacion determinada
 });
 
-
-function usuarioFavorito(id) {
-	let indice = id-1
-	let objeto_seleccionado = {}
-	objeto_seleccionado = user[indice]
-	favoritos.push(objeto_seleccionado)
-	localStorage.setItem("StorageFavoritos", JSON.stringify(favoritos));
-	console.log(favoritos)
-	contenedor_fav.innerHTML += `
-		<div class="card text-white bg-primary m-3"> 
-		  <div class="card-header"><img src=${objeto_seleccionado.avatar} /> ${objeto_seleccionado.first_name} ${objeto_seleccionado.last_name}</div>
-		  <div class="card-body">
-			<h4 class="card-title">Company: ${objeto_seleccionado.company}</h4>
-			<p class="card-text">job: ${objeto_seleccionado.company_department}</p>
-			<p class="card-text">email: ${objeto_seleccionado.email}</p>
-			<button onClick="quitarFavorito(${objeto_seleccionado.id})" class="btn btn-outline-danger">No Fav</button>
-		  </div>
-		</div>`; //cada tarjeta se va a generar ubicando las propiedades de cada objeto segun la obicacion determinada
-	
+function usuarioFavorito(identificador) {
+  let indice = identificador - 1;
+  let objeto_seleccionado = {};
+  objeto_seleccionado = user[indice];
+  if(!favoritos.some(e=>e.id===identificador)) {
+	  favoritos.push(objeto_seleccionado);
+	  localStorage.setItem("StorageFavoritos", JSON.stringify(favoritos));
+	  contenedor_fav.innerHTML += `
+			<div class="card text-white bg-primary m-3"> 
+			  <div class="card-header"><img src=${objeto_seleccionado.avatar} /> ${objeto_seleccionado.first_name} ${objeto_seleccionado.last_name}</div>
+			  <div class="card-body">
+				<h4 class="card-title">Company: ${objeto_seleccionado.company}</h4>
+				<p class="card-text">job: ${objeto_seleccionado.company_department}</p>
+				<p class="card-text">email: ${objeto_seleccionado.email}</p>
+				<button onClick="quitarFavorito(${objeto_seleccionado.id})" class="btn btn-outline-danger">No Fav</button>
+			  </div>
+			</div>`; //cada tarjeta se va a generar ubicando las propiedades de cada objeto segun la obicacion determinada
+  } else {
+	alert("el usuario ya se encuentra en favoritos =)")
+  }
 }
 
 function quitarFavorito(id) {
-	let favoritos_filtrado = favoritos.filter(elemento => elemento.id != id)
-	favoritos = favoritos_filtrado
-	localStorage.setItem("StorageFavoritos", JSON.stringify(favoritos));
-	console.log(favoritos_filtrado)
-	location.reload()
-
+  let favoritos_filtrado = favoritos.filter((elemento) => elemento.id != id);
+  favoritos = favoritos_filtrado;
+  localStorage.setItem("StorageFavoritos", JSON.stringify(favoritos));
+  console.log(favoritos_filtrado);
+  location.reload();
 }
-
-
-
-
