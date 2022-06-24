d6cbbf55e101e718ef93a5e20dc3d69d5a86f837
Jorge Angel PAEZ <jorgeangelpaez@outlook.com>
2022-06-24 12:23:48 -0300

    diagrama de flujo

--- AFTERCLASS/24062022/main.js
+++ AFTERCLASS/24062022/main.js
@@ -1,21 +1,46 @@
-//IDENTIFICA ELEMENTO CONTENEDOR O PADRE PARA INSERTAR TARJETAS
 let contenedor = document.getElementById("container");
-//GENERADOR DE TARJETAS DE USUARIOS
-user.forEach((user) => {
-  //BUCLE QUE REPASA LOS OBJETOS DE USER EN ARCHIVO usuarios.js
-  contenedor.innerHTML += `
-  <div class="card text-white bg-primary m-3"> 
-	<div class="card-header"><img src=${user.avatar} /> ${user.first_name} ${user.last_name}</div>
-	<div class="card-body">
-	  <h4 class="card-title">Company: ${user.company}</h4>
-	  <p class="card-text">job: ${user.company_department}</p>
-	  <p class="card-text">email: ${user.email}</p>
-	  <button onClick="usuarioFavorito(${user.id})" class="btn btn-outline-success">Fav</button>
-	</div>
-  </div>`; //cada tarjeta se va a generar ubicando las propiedades de cada objeto segun la obicacion determinada
-});
+let contenedor_fav = document.getElementById("container_fav");
+let favoritos = []; 
 
-//BUSCADOR DE USUARIOS
+cargarFavoritos();
+mostrarUsuarios();
+mostrarFavoritos();
+filtroUsuarios();
+
+
+///////////////////////////////////////FUNCIONES LLAMADAS AL CARGAR LA PAGINA////////////////////////////////////////////////
+
+//FUNCION CARGAR FAVORITOS GUARDADOS EN LOCALSTORAGE
+function cargarFavoritos() {
+  if (localStorage.getItem("StorageFavoritos") !== null) {
+    favoritos = JSON.parse(localStorage.getItem("StorageFavoritos"));
+    return;
+  } else {
+    localStorage.setItem("StorageFavoritos", JSON.stringify(favoritos));
+    return;
+  }
+}
+
+
+//FUNCION PARA CARGAR TARJETAS DE USUARIOS
+function mostrarUsuarios() {
+  user.forEach((user) => {
+    //BUCLE QUE REPASA LOS OBJETOS DE USER EN ARCHIVO usuarios.js
+    contenedor.innerHTML += `
+    <div class="card text-white bg-primary m-3"> 
+    <div class="card-header"><img src=${user.avatar} /> ${user.first_name} ${user.last_name}</div>
+    <div class="card-body">
+      <h4 class="card-title">Company: ${user.company}</h4>
+      <p class="card-text">job: ${user.company_department}</p>
+      <p class="card-text">email: ${user.email}</p>
+      <button onClick="usuarioFavorito(${user.id})" class="btn btn-outline-success">Fav</button>
+    </div>
+    </div>`; //cada tarjeta se va a generar ubicando las propiedades de cada objeto segun la obicacion determinada
+  });
+}
+
+
+//FUNCION PARA FILTRAR USUARIOS A PARTIR DE EVENTOS EN INPUT DE BUSQUEDA
 function filtroUsuarios() {
   const entrada = document.querySelector(".buscarUsuarios");
   entrada.addEventListener("keyup", (e) => {
@@ -34,39 +59,29 @@ function filtroUsuarios() {
   });
 }
 
-filtroUsuarios();
 
-let contenedor_fav = document.getElementById("container_fav");
-//FUNCION SELECTORA DE USUARIOS
 
-let favoritos = [];
-
-function cargarFavoritos() {
-  if (localStorage.getItem("StorageFavoritos") !== null) {
-    favoritos = JSON.parse(localStorage.getItem("StorageFavoritos"));
-    return;
-  } else {
-    localStorage.setItem("StorageFavoritos", JSON.stringify(favoritos));
-    return;
-  }
+//FUNCION MOSTRAR NUEVAS TARJETAS DE FAVORITOS
+function mostrarFavoritos () {
+  favoritos.forEach((favoritos) => {
+    contenedor_fav.innerHTML += `
+    <div class="card text-white bg-primary m-3"> 
+      <div class="card-header"><img src=${favoritos.avatar} /> ${favoritos.first_name} ${favoritos.last_name}</div>
+      <div class="card-body">
+      <h4 class="card-title">Company: ${favoritos.company}</h4>
+      <p class="card-text">job: ${favoritos.company_department}</p>
+      <p class="card-text">email: ${favoritos.email}</p>
+      <button onClick="quitarFavorito(${favoritos.id})" class="btn btn-outline-danger">No Fav</button>
+      </div>
+    </div>`; //cada tarjeta se va a generar ubicando las propiedades de cada objeto segun la obicacion determinada
+  });
 }
 
-cargarFavoritos();
 
-//GENERADOR DE TARJETAS DE FAVORITOS
-favoritos.forEach((favoritos) => {
-  contenedor_fav.innerHTML += `
-	<div class="card text-white bg-primary m-3"> 
-	  <div class="card-header"><img src=${favoritos.avatar} /> ${favoritos.first_name} ${favoritos.last_name}</div>
-	  <div class="card-body">
-		<h4 class="card-title">Company: ${favoritos.company}</h4>
-		<p class="card-text">job: ${favoritos.company_department}</p>
-		<p class="card-text">email: ${favoritos.email}</p>
-		<button onClick="quitarFavorito(${favoritos.id})" class="btn btn-outline-danger">No Fav</button>
-	  </div>
-	</div>`; //cada tarjeta se va a generar ubicando las propiedades de cada objeto segun la obicacion determinada
-});
 
+////////////////////////////////////////////////////////FUNCIONES A DEMANDA//////////////////////////////////////////////
+
+//FUNCION AGREGAR USUARIO FAVORITO AL ARRAY
 function usuarioFavorito(identificador) {
   let indice = identificador - 1;
   let objeto_seleccionado = {};
@@ -80,6 +95,7 @@ function usuarioFavorito(identificador) {
   }
 }
 
+//FUNCION QUITAR USUARIO FAVORITO AL ARRAY
 function quitarFavorito(id) {
   let favoritos_filtrado = favoritos.filter((elemento) => elemento.id != id);
   favoritos = favoritos_filtrado;
